d9678c061787eed25d9835ea0963f0a6b7c566da
Ameet Jadhav <ammugs11@gmail.com>
2022-08-25 10:37:46 +0530

    To get the name using the command line

---
+++ src/javapractice/main.java
@@ -0,0 +1,13 @@
+package javapractice;
+
+public class main {
+	
+	 public static void main(String[] args) {
+		    System.out.println("ameet jadhav 123");
+
+		    for(String str: args) {
+		      System.out.println(str);
+		    }
+		  }
+
+}
